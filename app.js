e636b4428ff2642606e0d72045a16b11d560ccf0
ibleedfilm <rem@lansangan.com>
2017-12-03 03:03:58 +0800

    :hammer: Refactoring with ES6

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 
-var steps = [];
+let steps = [];
 
 // Server index page
-app.get("/", function (req, res) {
+app.get('/', (req, res) => {
   res.send("Deployed!");
 });
 
 // Facebook Webhook
 // used for verification
-app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === "this_is_my_token") {
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.verify_token'] === 'this_is_my_token') {
       console.log("Verified webhook");
-      res.status(200).send(req.query["hub.challenge"]);
+
+      res.status(200).send(req.query['hub.challenge']);
   } else {
       console.error("Verification failed. The tokens do not match");
+
       res.sendStatus(403);
   }
 });
 
 // All callbacks for Messenger will be POST-ed here
-app.post("/webhook", function (req, res) {
+app.post('/webhook', (req, res) => {
   // Make sure this is a page subscription
-  if (req.body.object == "page") {
+  if (req.body.object === 'page') {
     // Iterate over each entry
     // There may be multiple entries if batched
-    req.body.entry.forEach(function(entry) {
+    req.body.entry.forEach(entry => {
       // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
+      entry.messaging.forEach(event => {
         if (event.postback) {
           processPostback(event);
         } else if (event.message) {
@@ -48,30 +51,33 @@ app.post("/webhook", function (req, res) {
 });
 
 function processPostback(event) {
-  var senderId = event.sender.id;
-  var payload = event.postback.payload;
+  const senderId = event.sender.id;
+  const payload = event.postback.payload;
 
-  if (payload === "Greeting") {
+  if (payload === 'Greeting') {
     // Get user's first name from the User Profile API
     // and include it in the greeting
     request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
+      url: `https://graph.facebook.com/v2.6/${senderId}`,
       qs: {
         access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
+        fields: 'first_name'
       },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
+      method: 'GET'
+    }, (error, response, body) => {
+      let greeting = "";
+
       if (error) {
-        console.log("Error getting user's name: " +  error);
+        console.log(`Error getting user's name: ${error}`);
       } else {
-        var bodyObj = JSON.parse(body);
-        name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
+        const bodyObj = JSON.parse(body);
+
+        greeting = `Hi ${bodyObj.first_name}.`;
       }
-      var message = greeting + "I am Inclusy, your intelligent loan officer bot. I can help you with loan and mortgage-related matters.";
-      sendMessage(senderId, {text: message});
+      
+      greeting = `${greeting} I am Inclusy, your intelligent loan officer bot. I can help you with loan and mortgage-related matters.`;
+
+      sendMessage(senderId, {text: greeting});
     });
   }
 }
@@ -79,37 +85,39 @@ function processPostback(event) {
 // sends message to user
 function sendMessage(recipientId, message) {
   request({
-    url: "https://graph.facebook.com/v2.6/me/messages",
+    url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: "POST",
+    method: 'POST',
     json: {
       recipient: {id: recipientId},
       message: message
     }
-  }, function(error, response, body) {
+  }, (error, response, body) => {
     if (error) {
-      console.log("Error sending message: " + response.error);
+      console.log(`Error sending message: ${response.error}`);
     }
   });
 }
 
 function processMessage(event) {
   if (!event.message.is_echo) {
-    var message = event.message;
-    var senderId = event.sender.id;
+    const message = event.message;
+    const senderId = event.sender.id;
 
-    console.log("Received message from senderId: " + senderId);
-    console.log("Message is: " + JSON.stringify(message));
+    console.log(`Received message from senderId: ${senderId}`);
+    console.log(`Message is: ${JSON.stringify(message)}`);
 
     if (message.text) {
-      var formattedMsg = message.text.toLowerCase().trim();
+      const formattedMsg = message.text.toLowerCase().trim();
 
       if (steps.length === 0 && hasKeyword(formattedMsg)) {
         sendMessage(senderId, {text: "May I clarify that you're asking for a loan? Please reply 'Yes' or 'No'."}, true);
+
         steps.push(true);
       } else if (steps[0] === true) {
           if (formattedMsg.includes('yes')) {
             sendMessage(senderId, {text: "May I ask how much?"});
+
             steps.push(true);
           } else {
             steps.push(false);
@@ -120,6 +128,7 @@ function processMessage(event) {
         } else {
           sendMessage(senderId, {text: "I'm sorry, but I can only assist you with loan-related matters."});
         }
+
         sendMessage(senderId, {text: "Thank you and have a nice day."});
       }
     }
